b294f6a13a4db897e9a4880f2c2bbb76f785d22c
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-06-19 01:45:52 +0600

    dsa

--- Module 9/delete_operations.cpp
+++ Module 9/delete_operations.cpp
@@ -46,34 +46,43 @@ void delete_at_position(Node* &head,int pos){
     temp->next->prev=temp;
     delete deleteNode;
 }
-void delete_tail(Node* &tail){
+void delete_tail(Node* &head,Node* &tail){
     Node* deletenode = tail;
     tail= tail->prev;
     delete deletenode;
+    if(tail == NULL){
+        head = NULL;
+        return;
+    }
     tail->next=NULL;
 }
-void delete_head(Node* &head){
+void delete_head(Node* &head,Node* &tail){
     Node* deleteNode = head;
     head =  head->next;
     delete deleteNode;
+    if(head == NULL){
+        tail = NULL;
+        return;
+    }
     head->prev=NULL;
 }
 int main(){
     // Node* head = NULL;
     // Node* tail = NULL;
     Node* head = new Node(10);
-    Node* a = new Node(20);
-    Node* b = new Node(30);
-    Node* c = new Node(40);
-    Node* tail = c;
+    Node* tail = head;
+    // Node* a = new Node(20);
+    // Node* b = new Node(30);
+    // Node* c = new Node(40);
+    // Node* tail = c;
 
 
-    head->next=a;
-    a->prev=head;
-    a->next=b;
-    b->prev=a;
-    b->next=c;
-    c->prev=b;
+    // head->next=a;
+    // a->prev=head;
+    // a->next=b;
+    // b->prev=a;
+    // b->next=c;
+    // c->prev=b;
 
     int pos;
     cin>>pos;
@@ -81,10 +90,10 @@ int main(){
         cout<<"Invalid"<<endl;
     }
     else if(pos == 0){
-        delete_head(head);
+        delete_head(head,tail);
     }
     else if(pos == size(head) -1 ){
-        delete_tail(tail);
+        delete_tail(head,tail);
     }
     else{
         delete_at_position(head,pos);
