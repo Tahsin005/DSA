4bc85bcff2e2680ace4a3dfaef497ac61d58030e
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-07-08 23:54:13 +0600

    dsa

---
+++ Module 17/pre_order_traversal.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+A subset of tree data structure that follows the only '2'
+method. Means it will maximum have two nodes/children if you will.
+*/
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+void preorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+int main(){
+    Node* root = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+    Node* e = new Node(60);
+    Node* f = new Node(70);
+    Node* g = new Node(80);
+    Node* h = new Node(90);
+    Node* i = new Node(100);
+
+    // Connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+    h->right = i;
+
+    /*
+    root -> left sub-tree -> right sub-tree
+    */
+    preorder(root);
+    return 0;
+}
